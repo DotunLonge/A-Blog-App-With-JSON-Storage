205f7f7de6d507dd72e7b6a1de9f3e8d78cf8a38
DotunLonge <funnica4@gmail.com>
2017-06-25 23:58:47 +0100

    Can Fetch Articles Commit

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,5 @@
    document.addEventListener("DOMContentLoaded", (event) => {
        let _JSON_URL = '../assets/JSON/articles.json';
-       let jsonText = null;
-
 
        let navbar = document.createElement('div')
        navbar.className = 'navBar'
@@ -14,18 +12,20 @@
        let body = document.body;
        body.appendChild(navbar)
 
-
        newPostButtton.addEventListener('click', () => {
            console.log('smart')
        });
 
+
        let fetchArticles = () => {
            let reader = new XMLHttpRequest();
-           reader.open("GET", _JSON_URL, true);
+           reader.open("GET", _JSON_URL, false);
+
            reader.onreadystatechange = () => {
-               reader.readyState == 4 ?
-                   reader.status == 200 || reader.status == 0 ?
-                   jsonText = reader.responseText : console.log("could not read json") : console.log('reader.readyState != 4');
+               if (reader.readyState == 4) {
+                   var articles = JSON.parse(reader.responseText);
+
+               }
            }
            reader.send();
 
