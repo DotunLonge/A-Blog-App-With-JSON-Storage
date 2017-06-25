045bab6869fdfaa320c1515d1a1f0edba1c61b40
DotunLonge <funnica4@gmail.com>
2017-06-26 00:24:47 +0100

    Part Success in Retrieving Data Commit

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,16 +1,17 @@
    document.addEventListener("DOMContentLoaded", (event) => {
        let _JSON_URL = '../assets/JSON/articles.json';
-
        let navbar = document.createElement('div')
+       var articles;
        navbar.className = 'navBar'
 
        let newPostButtton = document.createElement('button')
        newPostButtton.className = 'newPostButton'
        newPostButtton.innerHTML = 'New Post'
-       navbar.appendChild(newPostButtton)
 
        let body = document.body;
+
        body.appendChild(navbar)
+       navbar.appendChild(newPostButtton)
 
        newPostButtton.addEventListener('click', () => {
            console.log('smart')
@@ -23,15 +24,62 @@
 
            reader.onreadystatechange = () => {
                if (reader.readyState == 4) {
-                   var articles = JSON.parse(reader.responseText);
-
+                   articles = JSON.parse(reader.responseText);
                }
            }
            reader.send();
+       }
+       fetchArticles();
+
+
+       let construct = (titleData, descriptionData, datePublishedData, articleImageData, publishedByData) => {
+           let holder = document.createElement('div')
+           holder.className = 'xs-12 sm-6 md-4 holder'
+
+           insideThings = document.createElement('div')
+           insideThings.className = 'insideThing'
+
+           let title = document.createElement('h4')
+           title.className = 'title';
+           title.innerHTML = titleData;
+
+           let description = document.createElement('div')
+           description.className = 'description';
+           description.innerHTML = descriptionData.substring(0, 10);
+
+           let articleImage = document.createElement('div')
+           articleImage.className = 'articleImage';
+           articleImage.style.backgroundImage = `url${articleImageData}`;
+
+           let datePublished = document.createElement('h5')
+           datePublished.className = 'datePublished'
+           datePublished.innerHTML = datePublishedData;
+
+           let publishedBy = document.createElement('h5')
+           publishedBy.className = 'publishedBy';
+           publishedBy.innerHTML = publishedByData;
+
+
+           body.appendChild(holder)
+           holder.appendChild(insideThings);
+           insideThings.appendChild(title);
+           insideThings.appendChild(description);
+           insideThings.appendChild(articleImage)
+           insideThings.appendChild(datePublished)
+           insideThings.appendChild(publishedBy)
 
        }
 
-       fetchArticles();
+       for (each in articles) {
+           let Title = articles[each].Title;
+           let Description = articles[each].Description;
+           let datePublished = articles[each].datePublished;
+           let articleImage = articles[each].articleImage;
+           let publishedBy = articles[each].publishedBy;
+
+           construct(Title, Description, datePublished, articleImage, publishedBy);
+
+       }
 
 
 
